6ef3f547c4564da382ede2e1dfc6b6fb8ff8567d
MD Shakib <mdsihabsharar22@gmail.com>
2026-09-11 17:10:31 +0600

    Add Loading state for Technology data

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import AvailableStacks from './components/availableStacks'
 import type { TechTypes } from './types/types'
 
 const ItemFetch = async (): Promise<TechTypes[]> => {
+
+  await new Promise(resolve => setTimeout(resolve, 1000));
   const response = await fetch('../data.json');
   const data = await response.json();
   return data;
@@ -17,7 +19,7 @@ const ItemFetch = async (): Promise<TechTypes[]> => {
 
 function App() {
   const [techPromise] = useState(() => ItemFetch());
-  const [selected, setSelectedCard] =  useState<TechTypes[]>([])
+  const [selected, setSelectedCard] = useState<TechTypes[]>([])
 
   return (
     <>
@@ -25,14 +27,16 @@ function App() {
         <Navbar />
         <Banner />
         <ExploreSide />
-        <Suspense fallback={<h2>Loading...</h2>}>
+        <Suspense fallback={<div className="flex justify-center items-center py-20">
+          <span className="loading loading-spinner loading-xl"></span>
+        </div>}>
           <AvailableStacks
             techPromise={techPromise}
-              selected={selected}
+            selected={selected}
             setSelectedCard={setSelectedCard}
-             />
+          />
 
-          
+
         </Suspense>
         <Footer />
 
